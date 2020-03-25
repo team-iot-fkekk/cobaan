1e7b0e33d553c46bc8e1838b96b5ee645db1c189
team-iot-fkekk <team.iot.fkekk@gmail.com>
2020-03-26 00:19:39 +0800

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(6,HIGH);
-  delay(1000);
-  digitalWrite(6,LOW);
-  delay(1000);
+  digitalWrite(6,HIGH); // turn ON LED
+  delay(1000);          // delay 1s
+  digitalWrite(6,LOW);  // turn OFF LED
+  delay(1000);          // delay 1s
 }
